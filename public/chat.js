27d41c8a4868b234e2a5b540969995c365df3b3c
JordiVargasRuiz <jordi.vargas3669@alumnos.udg.mx>
2025-02-11 17:25:04 -0600

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://fastwebchat-79ab091e9149.herokuapp.com');
 
 // Query DOM
 var message = document.getElementById('message');
