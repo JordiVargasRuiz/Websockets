d3806015d63437e0cc2714ced2d2a497419037d8
JordiVargasRuiz <jordi.vargas3669@alumnos.udg.mx>
2025-02-11 18:43:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var socket = require('socket.io');
+var cors = require('cors');
 
 // App setup
 var app = express();
 var PORT = process.env.PORT || 4000;
 
+// Habilitar CORS solo para el dominio de tu aplicación en Heroku
+app.use(cors({
+    origin: "https://fastweb-29361be2e352.herokuapp.com",
+    methods: ["GET", "POST"]
+}));
+
 var server = app.listen(PORT, function () {
     console.log(`Listening for requests on port ${PORT}`);
 });
 
-// Static files
+// Servir archivos estáticos
 app.use(express.static('public'));
 
-// Socket setup
-var io = socket(server);
+// Configurar Socket.io con CORS permitido solo para Heroku
+var io = socket(server, {
+    cors: {
+        origin: "https://fastweb-29361be2e352.herokuapp.com",
+        methods: ["GET", "POST"]
+    }
+});
 
 io.on('connection', function(socket){
     console.log('Made socket connection', socket.id);
